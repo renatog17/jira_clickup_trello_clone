18d9b5c2b7590442b07f944e254c4a9b54a7d8c1
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-09-15 22:51:29 -0300

    creating tests methods

--- agileflow/src/test/java/com/renato/agileflow/controllers/ProjectControllerTest.java
+++ agileflow/src/test/java/com/renato/agileflow/controllers/ProjectControllerTest.java
@@ -1,58 +1,48 @@
 package com.renato.agileflow.controllers;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.containsString;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.renato.agileflow.controllers.dto.CreateProjectDTO;
-import com.renato.agileflow.domain.Project;
-import com.renato.agileflow.repositories.ProjectRepository;
 
-@SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
 public class ProjectControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
-	
-	@MockBean
-	private ProjectRepository projectRepository;
-	
-	@Test
-	@DisplayName("Deve criar um novo projeto")
-	public void postProjectTest() throws Exception {
-		CreateProjectDTO createProjectDTO = new CreateProjectDTO("Projeto teste", "descrição teste", LocalDate.of(2024, 8, 12), null);
-		when(projectRepository.save(any(Project.class))).thenReturn(new Project(createProjectDTO));
-		
-		  mockMvc.perform(post("/project")
-		            .contentType(MediaType.APPLICATION_JSON)
-		            .content("{\"name\": \"Projeto teste\", \"description\": \"descrição teste\", \"startDate\": \"2024-08-12\", \"endDate\": null}"))
-		            .andExpect(status().isCreated());
-	}
-	
-	@Test
-	@DisplayName("Deve retornar erro 400 ao tentar criar um novo projeto")
-	public void postProjectTest400() throws Exception {
-		CreateProjectDTO createProjectDTO = new CreateProjectDTO("Projeto teste", "descrição teste", LocalDate.of(2024, 8, 12), null);
-		when(projectRepository.save(any(Project.class))).thenReturn(new Project(createProjectDTO));
-		
-		  mockMvc.perform(post("/project")
-		            .contentType(MediaType.APPLICATION_JSON)
-		            .content("{\"name\": \"\", \"description\": \"descrição teste\", \"startDate\": \"2024-08-12\", \"endDate\": null}"))
-		            .andExpect(status().isCreated());
-	}
-	
+	 @Autowired
+	    private MockMvc mockMvc;
+
+	    @Autowired
+	    private ObjectMapper objectMapper;
+
+	    @Test
+	    public void testPostProject() throws Exception {
+	        // Arrange
+	        CreateProjectDTO projectDTO = new CreateProjectDTO(
+	                "New Project",
+	                "Project description",
+	                LocalDate.now(),
+	                LocalDate.now().plusDays(10)
+	        );
+
+	        // Act & Assert
+	        mockMvc.perform(MockMvcRequestBuilders.post("/project")
+	                .contentType(MediaType.APPLICATION_JSON)
+	                .content(objectMapper.writeValueAsString(projectDTO)))
+	                .andExpect(MockMvcResultMatchers.status().isCreated())
+	                .andExpect(MockMvcResultMatchers.header().string("Location", containsString("/project/2")));
+	    }
 	
 }
