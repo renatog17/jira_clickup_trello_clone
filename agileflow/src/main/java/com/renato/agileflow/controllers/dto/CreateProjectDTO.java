a4e1b00526b8467e1c9fa31571897c3ce92b44db
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-09-16 14:57:32 -0300

    tests post project created

--- agileflow/src/main/java/com/renato/agileflow/controllers/dto/CreateProjectDTO.java
+++ agileflow/src/main/java/com/renato/agileflow/controllers/dto/CreateProjectDTO.java
@@ -2,13 +2,14 @@ package com.renato.agileflow.controllers.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 public record CreateProjectDTO(
-		@NotNull(message = "tha name cannot be null")
+		@NotEmpty(message = "tha name cannot be null")
 		String name,
-		@NotNull(message = "the description cannot be null")
+		@NotEmpty(message = "the description cannot be null")
 		String description,
 		@NotNull(message = "the start date cannot be null")
 		@PastOrPresent
--- agileflow/src/test/java/com/renato/agileflow/controllers/ProjectControllerTest.java
+++ agileflow/src/test/java/com/renato/agileflow/controllers/ProjectControllerTest.java
@@ -1,5 +1,4 @@
 package com.renato.agileflow.controllers;
-
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
@@ -11,38 +10,75 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.hamcrest.Matchers.containsString;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.renato.agileflow.controllers.dto.CreateProjectDTO;
+import com.renato.agileflow.repositories.ProjectRepository;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
 public class ProjectControllerTest {
 
-	 @Autowired
-	    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-	    @Autowired
-	    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-	    @Test
-	    public void testPostProject() throws Exception {
-	        // Arrange
-	        CreateProjectDTO projectDTO = new CreateProjectDTO(
-	                "New Project",
-	                "Project description",
-	                LocalDate.now(),
-	                LocalDate.now().plusDays(10)
-	        );
+	@Autowired
+	private ProjectRepository projectRepository;
 
-	        // Act & Assert
-	        mockMvc.perform(MockMvcRequestBuilders.post("/project")
-	                .contentType(MediaType.APPLICATION_JSON)
-	                .content(objectMapper.writeValueAsString(projectDTO)))
-	                .andExpect(MockMvcResultMatchers.status().isCreated())
-	                .andExpect(MockMvcResultMatchers.header().string("Location", containsString("/project/2")));
-	    }
 	
+	@Test
+	public void testPostProject() throws Exception {
+		// Arrange
+		CreateProjectDTO projectDTO = new CreateProjectDTO("New Project", "Project description", LocalDate.now(),
+				LocalDate.now().plusDays(10));
+
+		// Act & Assert
+		mockMvc.perform(MockMvcRequestBuilders.post("/project").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(projectDTO)))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
+				//.andExpect(MockMvcResultMatchers.header().string("Location", containsString("/project/2")));
+	}
+
+	@Test
+	public void testPostProjectWithEmptyFields() throws JsonProcessingException, Exception {
+		// Arrange
+		CreateProjectDTO projectDTOEmptyField = new CreateProjectDTO("", "Project description", LocalDate.now(),
+				LocalDate.now().plusDays(10));
+
+		CreateProjectDTO projectDTONullField = new CreateProjectDTO(null, "Project description", LocalDate.now(),
+				LocalDate.now().plusDays(10));
+
+		CreateProjectDTO projectDTONullStartDate = new CreateProjectDTO("Test", "Project description", null,
+				LocalDate.now().plusDays(10));
+
+		CreateProjectDTO projectDTONullEndDate = new CreateProjectDTO("Test", "Project description", LocalDate.now(),
+				null);
+
+		// Act & Assert
+		mockMvc.perform(MockMvcRequestBuilders.post("/project").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(projectDTOEmptyField)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+		// Act & Assert
+		mockMvc.perform(MockMvcRequestBuilders.post("/project").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(projectDTONullField)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+		// Act & Assert
+		mockMvc.perform(MockMvcRequestBuilders.post("/project").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(projectDTONullStartDate)))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+		// Act & Assert
+		mockMvc.perform(MockMvcRequestBuilders.post("/project").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(projectDTONullEndDate)))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
+				//.andExpect(MockMvcResultMatchers.header().string("Location", containsString("/project/1")));
+	}
+
 }
