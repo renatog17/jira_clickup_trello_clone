83efb2969e45ce81c738d65d127cc832f5aebf38
Renato Oliveira <renatog.oliveira17@gmail.com>
2024-09-17 08:52:33 -0300

    get project test created

--- agileflow/src/test/java/com/renato/agileflow/controllers/ProjectControllerTest.java
+++ agileflow/src/test/java/com/renato/agileflow/controllers/ProjectControllerTest.java
@@ -14,22 +14,24 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.renato.agileflow.controllers.dto.CreateProjectDTO;
+import com.renato.agileflow.domain.Project;
 import com.renato.agileflow.repositories.ProjectRepository;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
 public class ProjectControllerTest {
-
+//O banco não está sendo meio que resetado, ou seja, o id não começa do zero e eu ainda estou 
+//	pensando em uma maneira de fazer isso
+	
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
 	private ObjectMapper objectMapper;
-
+	
 	@Autowired
 	private ProjectRepository projectRepository;
-
 	
 	@Test
 	public void testPostProject() throws Exception {
@@ -80,5 +82,19 @@ public class ProjectControllerTest {
 				.andExpect(MockMvcResultMatchers.status().isCreated());
 				//.andExpect(MockMvcResultMatchers.header().string("Location", containsString("/project/1")));
 	}
-
+	
+	@Test
+	public void testGetProject() throws Exception {
+		Project project = new Project("Teste Get", "Descrição teste get", LocalDate.now(), null);
+		project = projectRepository.save(project);
+		System.out.println(project.getId()+"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		//Antes eu preciso inserir um objeto no banco de dados, pegar o id desse objeto e passa como path variable
+		//na url
+		//Act & Assert
+		mockMvc.perform(MockMvcRequestBuilders.get("/project/"+project.getId()))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/project/9999"))
+		.andExpect(MockMvcResultMatchers.status().isNotFound());
+	}
 }
